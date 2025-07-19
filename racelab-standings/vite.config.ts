9130ef4d5f79fdc9b275f01c9d84319778ce6e0c
Alexander-2049 <a.alexander.2049@gmail.com>
2025-07-20 01:41:54 +0300

    chore: update dependencies and configure Vite for TypeScript declaration generation
    - Added vite-plugin-dts to package.json for TypeScript declaration file generation.
    - Removed empty vite-env.d.ts file.
    - Updated vite.config.ts to include vite-plugin-dts and configured it to exclude CSS modules from declaration generation.

--- racelab-standings/vite.config.ts
+++ racelab-standings/vite.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import { viteStaticCopy } from "vite-plugin-static-copy"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     viteStaticCopy({
-      targets: [
-        {
-          src: "manifest.json",
-          dest: ".",
-        },
-      ],
+      targets: [{ src: "manifest.json", dest: "." }],
+    }),
+    dts({
+      exclude: ["**/*.module.css"], // <-- prevent d.ts generation for CSS modules
     }),
   ],
   base: "",
-})
+});
